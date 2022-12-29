0cb4d1affc8acb93380a2cfdec625f2b636a6699
MrSemyon12 <denisenko.box19@gmail.com>
2022-12-29 17:39:31 +1000

    flask skeleton

--- code/app.py
+++ code/app.py
@@ -1,1 +1,16 @@
-print('running app...')
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    input = request.args.get('input')
+    with open(r'code/translator/output.txt', 'r') as file:
+        output = file.read()
+    print(output)
+    return render_template('index.html', input=input, output=output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
