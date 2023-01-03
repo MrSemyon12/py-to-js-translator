40c0237fb07a071039f134e823f0ac836fa0ef05
MrSemyon12 <denisenko.box19@gmail.com>
2023-01-04 01:01:02 +1000

    -

--- code/app.py
+++ code/app.py
@@ -18,16 +18,16 @@ def index():
     if not input:
         return render_template('index.html', input='', output=output, operationTree=operationTree, syntaxTree=syntaxTree)
 
-    with open('code/translator/input.txt', 'w') as file:
+    with open(path.join(basedir, 'translator/input.txt'), 'w') as file:
         file.write(input)
 
     try:
         test(path.join(basedir, 'translator/'))
-        with open('code/translator/output.txt', 'r') as file:
+        with open(path.join(basedir, 'translator/output.txt'), 'r') as file:
             output = file.read()
-        with open('code/translator/outputOperationTree.txt', 'r') as file:
+        with open(path.join(basedir, 'translator/outputOperationTree.txt'), 'r') as file:
             operationTree = file.read()
-        with open('code/translator/outputSyntaxTree.txt', 'r') as file:
+        with open(path.join(basedir, 'translator/outputSyntaxTree.txt'), 'r') as file:
             syntaxTree = file.read()
     except Exception as err:
         flash(f"{err}", category='error')
